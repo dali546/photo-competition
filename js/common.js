a62daf207f6367acefbb7ebd010b850ea7392ede
dali546 <35352237+dali546@users.noreply.github.com>
2019-04-24 10:29:39 +0100

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 'use strict';
 // JavaScript containing shared constants, for use in all pages.
 
-var backendIp = 'backend IP goes here';
-var token = 'your token goes here';
+var backendIp = 'ec2-99-81-194-20.eu-west-1.compute.amazonaws.com';
+var token = '651b08e0-8360-46a9-aea7-12af4b45a3c0';
 
 function buildUrl(path) {
     return 'http://' + backendIp + '/images' + path + '?token=' + token;
